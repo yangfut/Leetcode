e6f4959f5a3700871080c363f8496109cddfe831
Daniel Tseng <49602315+yangfut@users.noreply.github.com>
2021-02-07 00:25:44 +0800

    Update 957_Prison-Cells-After-N-Days.py

--- python/957_Prison-Cells-After-N-Days.py
+++ python/957_Prison-Cells-After-N-Days.py
@@ -39,4 +39,34 @@ def main():
 if __name__ == '__main__':
     main()
 
-
+'''
+Reference to https://leetcode.com/problems/prison-cells-after-n-days/discuss/861468/Python-pigeonhole-principle-with-comment
+class Solution:
+    def prisonAfterNDays(self, cells: List[int], N: int) -> List[int]:
+		# seen record already seen config from day 1, mod is the cycle days, start_loop_idx is from which seen[] index it starts to repeat itself
+        seen = []
+        mod, start_loop_idx = -1, -1
+		# find the cycle
+        for i in range(1, N+1):
+            new_cell = [False] * 8
+            for i in range(1, 7):
+                new_cell[i] = not (cells[i-1] ^ cells[i+1])
+            if new_cell in seen:
+                start_loop_idx = seen.index(new_cell)
+                mod = len(seen) - start_loop_idx
+                break
+            else:
+                seen.append(new_cell)
+            cells = new_cell
+        
+		# After knowing the cycle, we lookup list seen[] to find the answer we want
+        r = N % mod
+        index = start_loop_idx + r-1 if r-1>=0 else len(seen) -1
+        res = seen[index]
+        for i, v in enumerate(res):
+            if v == True:
+                res[i] = 1
+            else:
+                res[i] = 0
+        return res
+'''
