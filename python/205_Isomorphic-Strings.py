57b735aedc74405ba087cb37dc79ef9ef290c5c6
Daniel_Tseng <yangfut@uci.edu>
2023-02-11 17:02:19 +0800

    [205][Python] Update another solution for Isomorphic Strings

--- python/205_Isomorphic-Strings.py
+++ python/205_Isomorphic-Strings.py
@@ -13,13 +13,32 @@ class Solution:
                 if hashmap[ord(ele)] != t[idx]:
                     return False
         return True
+    def isIsomorphic2(self, s: str, t: str) -> bool:
+        #Check alphabet is used
+        #Check every single word is being mapped
+        #Check no word is mapped to the same target
+
+        charMap = {}
+        IsTargetUsed = [0] * 128
+        for idx, ele in enumerate(s):
+            if ele in charMap:
+                if charMap[ele] != t[idx]:
+                    return False
+            else:
+                if IsTargetUsed[ord(t[idx])] == 0:
+                    IsTargetUsed[ord(t[idx])] += 1
+                else:
+                    return False
+                charMap[ele] = t[idx]
+                
+        return True
 if __name__ == "__main__":
     sol = Solution()
     
     s = "badc"
     t = "baba"
-    print(sol.isIsomorphic(s, t))
+    print(sol.isIsomorphic2(s, t))
 
     s = "paper"
     t = "title"
-    print(sol.isIsomorphic(s, t))
+    print(sol.isIsomorphic2(s, t))
