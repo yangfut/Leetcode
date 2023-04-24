d3bfadc280d6f59ea0661b07b6b43f6095c70b81
Daniel_Tseng <yangfut@uci.edu>
2023-04-24 23:06:33 +0800

    [3][c] Update Sliding Window solution

--- c/3_Longest-Substring-Without-Repeating-Characters.c
+++ c/3_Longest-Substring-Without-Repeating-Characters.c
@@ -1,4 +1,39 @@
 int lengthOfLongestSubstring(char * s){
+    int lengthOfLongestSubstring(char *s){
+	
+    //Sliding window
+    int *asciiMap;
+    char *st = s;
+    char *ed = s;
+    int n = strlen(s);
+    int maxLen = 0;
+    asciiMap = calloc(128, sizeof(int));
+
+    while(ed < s+n) {
+        
+        //Valid addon
+        if (asciiMap[*ed] == 0) {
+            //Expand the window
+            asciiMap[*ed] = 1;
+            ed++;
+            if ((ed - st) > maxLen){
+                maxLen = ed - st;
+            }
+        } else {
+            //Strink the window
+            asciiMap[*st] = 0;
+            st++;
+        }
+    }
+    free(asciiMap);
+    return maxLen;
+}
+
+    
+    
+    
+    //Naive approach
+    /*
     if (*s=='\0') return 0;
     int asciiMap[127] = {0};
     int idx = 0;
@@ -21,4 +56,6 @@ int lengthOfLongestSubstring(char * s){
         head++;
     }
     return maxSubLen;
+    */
+
 }
