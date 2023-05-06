5cbf09d10d9e0a6cb3cf4f2408a0170c0f2c44cd
Daniel_Tseng <yangfut@uci.edu>
2023-05-06 15:35:17 +0800

    [494][C++] Target Sum

--- c++/494_Target-Sum.cpp
+++ c++/494_Target-Sum.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-      
-	int n = nums.size();
-	int sum = 0;
-	for(int ele : nums){
-	    sum += ele;
-    }
+        int sum = 0;
+        for(int ele : nums){
+            sum+=ele;
+        }
+        if(target > sum || target < -sum) return 0;
+        int diff = sum - target;
 
-    if(target < -sum || target > sum) return 0;
+        vector<vector<int>>dp(nums.size(), vector<int>(diff+1, 0));
+        dp[0][diff] = 1;
+        if(diff - 2*nums[0]>=0)
+            dp[0][diff - 2*nums[0]] += 1;
 
-    int offset = sum;
-    vector<vector<int>>dp(n, vector<int>(2*sum+1, 0));
-
-    dp[0][nums[0]+offset]=1;
-    dp[0][offset-nums[0]]+=1;
-
-    for(int idx = 1; idx < n; idx++){
-        for(int s=0; s <= sum+offset; s++){
-            if(dp[idx-1][s]>0){
-                dp[idx][s-nums[idx]] += dp[idx-1][s];
-                dp[idx][s+nums[idx]] += dp[idx-1][s];
+        for(int idx = 1; idx < nums.size(); ++idx){
+            for(int d = diff; d>=0; --d){
+                if(d + 2*nums[idx] <= diff){
+                dp[idx][d] = dp[idx-1][d + 2*nums[idx]] + dp[idx-1][d];
+                }else{
+                    dp[idx][d] = dp[idx-1][d];
+                }
             }
         }
-    }
 
-    return dp[n-1][target+offset];
+        return dp[nums.size()-1][0];
     }
 };
