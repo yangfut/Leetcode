4a6193668e48447f430b12759e40ad0c02bce2be
Daniel_Tseng <yangfut@uci.edu>
2023-11-22 00:46:12 +0800

    [144][C++] Binary Tree Preorder Traversal (Two Stacks)

--- c++/144_Binary-Tree-Preorder-Traversal.cpp
+++ c++/144_Binary-Tree-Preorder-Traversal.cpp
@@ -11,19 +11,51 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode *node, vector<int> *vi){
-        if(node == NULL) return;
-        vi->push_back(node->val);
-        dfs(node->left, vi);
-        dfs(node->right, vi);
-    }
-    vector<int> preorderTraversal(TreeNode* root) {
-        if(root == NULL) return vector<int>();
+    //Solution: Recursion
+    /*
+    void dfs_bottom(TreeNode* node, vector<int>* vi) {
+        if(!node) return;
 
-        vector<int>vi;
+        dfs_bottom(node->left, vi);
+        dfs_bottom(node->right, vi);
+        vi->push_back(node->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>vi;    
+        if(!root) return vi;
+        
+        dfs_bottom(root->left, &vi);
+        dfs_bottom(root->right, &vi);
         vi.push_back(root->val);
-        dfs(root->left, &vi);
-        dfs(root->right, &vi);
+        return vi;
+    }
+    */
+    //Solution: Two Stacks
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>vi;    
+        if(!root) return vi;
+        
+        stack<TreeNode*> s1, s2;
+        s1.push(root);
+
+        while(!s1.empty()){
+            TreeNode *cur = s1.top();
+            s1.pop();
+            s2.push(cur);
+
+            if(cur->left != nullptr){
+                s1.push(cur->left);
+            }
+            if(cur->right != nullptr){
+                s1.push(cur->right);
+            }
+        }
+
+        while(!s2.empty()){
+            vi.push_back(s2.top()->val);
+            s2.pop();
+        }
+
         return vi;
     }
 };
