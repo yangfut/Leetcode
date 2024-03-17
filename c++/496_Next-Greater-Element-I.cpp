5b468527cd5de61989fcb18d3668179846f25136
Daniel_Tseng <yangfut@uci.edu>
2024-03-17 22:49:46 +0800

    [496][C++] Next Greater Element I

---
+++ c++/496_Next-Greater-Element-I.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        //Monotonic stack(monotonic decreasing from the back side)
+        stack<int>st;
+        unordered_map<int,int>map;
+        vector<int>res(nums1.size());
+        
+        for(int i=0;i<nums1.size();++i) map[nums1[i]]=i;
+        for(int i=nums2.size()-1;i>=0;--i){
+            
+            //Create monotonic descreasing order in stack
+            //Pop element if nums[i] is greater to keep the order in decreasing
+            while(!st.empty()&&nums2[i]>nums2[st.top()]) st.pop();
+
+            if(map.count(nums2[i])){
+                int idx_nums1 = map[nums2[i]];
+                res[idx_nums1] = st.empty()?-1:nums2[st.top()];
+            }
+            st.push(i);
+        }
+        return res;
+    }
+};
