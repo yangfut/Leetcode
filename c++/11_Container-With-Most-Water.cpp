5e910427cf752b55d3d0af7aba61e090db6cdb22
Daniel_Tseng <yangfut@uci.edu>
2024-03-20 00:10:24 +0800

    [11][C++] Update solution

--- c++/11_Container-With-Most-Water.cpp
+++ c++/11_Container-With-Most-Water.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int left=0;
-        int right=height.size()-1;
-        int area=0;
-
-        while(left<right){
-            area=max(area, (right-left)*min(height[left], height[right]));
-
-            if(height[left]<height[right]) left++;
-            else right--;
+        int n=height.size();
+        int l=0, r=n-1, area=0;
+        while(l<r){
+            int temp_area = (r-l) * min(height[l], height[r]);
+            if(temp_area>area) area = temp_area;
+            if(height[l]>height[r]) --r;
+            else ++l;
         }
         return area;
     }
