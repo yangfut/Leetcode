c897544e5693b3d2a61e0eacf757bc7e39f90599
Daniel_Tseng <yangfut@uci.edu>
2024-03-29 01:12:39 +0800

    [239][C++] Update double-ended queue solution

--- c++/239_Sliding-Windows-Maximum.cpp
+++ c++/239_Sliding-Windows-Maximum.cpp
@@ -68,3 +68,58 @@ public:
         return pq_sol(nums, k);
     }
 };
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // Use double-ended queue(deq) to track the maximum value in window
+        // deq.front() is the maximum value
+        // if input element > deq.back(), deq.pop_back() until deq.empty() or deq.back() is greater
+
+        //Edge cases
+        if(nums.size()==1) return nums;
+        deque<int>deq;
+        int n=nums.size();
+        vector<int>output(n-k+1,0);
+        
+        //Initial deq
+        for(int st=0;st<k;++st){
+            while(!deq.empty()&&deq.back()<nums[st]){
+                deq.pop_back();
+            }
+            deq.push_back(nums[st]);
+        }
+        output[0]=deq.front();
+        for(int st=1; st<=n-k;++st){
+            if(deq.front()==nums[st-1]) deq.pop_front();
+            while(!deq.empty()&&deq.back()<nums[st+k-1]) deq.pop_back();
+            deq.push_back(nums[st+k-1]);
+            output[st]=deq.front();
+        }
+        return output;
+    }
+};
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // Use double-ended queue(deq) to track the maximum value in window
+        // deq.front() is the maximum value
+        // if input element > deq.back(), deq.pop_back() until deq.empty() or deq.back() is greater
+
+        //Edge cases
+        if(nums.size()==1) return nums;
+        deque<int>deq;
+        int n=nums.size();
+        vector<int>output(n-k+1,0);
+        
+        for(int idx=0; idx<n;++idx){
+
+            if(!deq.empty()&&deq.front()==idx-k) deq.pop_front();
+            while(!deq.empty()&&nums[deq.back()]<=nums[idx]) deq.pop_back();
+            deq.push_back(idx);
+            if(idx>=k-1) output[idx-k+1]=nums[deq.front()];
+        }
+        return output;
+    }
+};
