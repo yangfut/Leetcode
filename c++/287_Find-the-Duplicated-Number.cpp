829ae05530c53979261ca70bc07f5bf30708b879
Daniel_Tseng <yangfut@uci.edu>
2024-03-30 21:33:14 +0800

    [287][C++] Update Floyd's Tortoise and Hare solution

--- c++/287_Find-the-Duplicated-Number.cpp
+++ c++/287_Find-the-Duplicated-Number.cpp
@@ -86,3 +86,23 @@ public:
         return floyed_algorithm_solution(nums);
     }
 };
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=0;
+        int fast=0;
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow!=fast);
+        
+        slow=0;
+        do{
+            slow = nums[slow];
+            fast = nums[fast];
+        }while(slow!=fast);
+
+        return slow;
+    }
+};
