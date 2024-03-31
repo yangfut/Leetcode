faaeaf2dbf979a5e43a8fe089ace9c787a99263d
Daniel_Tseng <yangfut@uci.edu>
2024-03-31 18:49:41 +0800

    [110][C++] Update optimized solution

--- c++/110_Balanced-Binary-Tree.cpp
+++ c++/110_Balanced-Binary-Tree.cpp
@@ -33,3 +33,33 @@ public:
         return (abs(right - left) <= 1) ? true : false;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool returnIsBalanced=true;
+    int getDepth(TreeNode* curNode){
+        if(curNode==nullptr||!returnIsBalanced) return 0;
+
+        int ld = getDepth(curNode->left);
+        int rd = getDepth(curNode->right);
+
+
+        if(abs(rd-ld)>1) returnIsBalanced = false;
+        return max(ld,rd)+1;
+    }
+    bool isBalanced(TreeNode* root) {
+        getDepth(root);
+        return returnIsBalanced;
+    }
+};
