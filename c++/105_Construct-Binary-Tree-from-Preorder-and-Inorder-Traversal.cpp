9a5fa44cde014d64a80d46dbb6396aecb131ae1a
Daniel_Tseng <yangfut@uci.edu>
2024-04-04 02:47:47 +0800

    [105][C++] Update two solutions for this question

--- c++/105_Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.cpp
+++ c++/105_Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.cpp
@@ -44,3 +44,64 @@ public:
         return root;
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // #1 Recursion
+    TreeNode* _build(vector<int>& preorder, vector<int>& inorder, int preStart, int preEnd, int inStart, int inEnd, unordered_map<int,int>& mp){
+        // end case
+        if(preStart > preEnd || inStart > inEnd) return nullptr;
+        
+        int inNode = mp[preorder[preStart]];
+        int leftNum = inNode - inStart;
+        TreeNode* curNode = new TreeNode(preorder[preStart]);
+        curNode->left = _build(preorder, inorder, preStart+1, preEnd, inStart, inNode-1, mp);
+        curNode->right = _build(preorder, inorder, preStart+leftNum+1, preEnd, inNode+1, inEnd, mp);
+        return curNode;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        unordered_map<int,int>mp;
+        int n=preorder.size();
+        for(int i=0;i<inorder.size();++i) mp[inorder[i]]=i;
+
+        int preStart=0, preEnd=n-1;
+        int inStart=0, inEnd=n-1;
+        return _build(preorder, inorder, preStart, preEnd, inStart, inEnd, mp);
+    }
+    // #2 Update the vector of preorder and inorder
+    // Reconstruct preorder and inorder to build subtree
+    // TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    //     //base cases
+    //     if(preorder.empty()) return nullptr;
+        
+    //     int idx;
+    //     for(int i=0;i<inorder.size();++i){
+    //         if(inorder[i]==preorder[0]){
+    //             idx=i;
+    //             break;
+    //         }
+    //     }
+    //     TreeNode* root = new TreeNode(preorder[0]);
+    //     vector<int> sub_left_preorder(preorder.begin()+1, preorder.begin()+1+idx);
+    //     vector<int> sub_left_inorder(inorder.begin(), inorder.begin()+idx);
+    //     root->left = buildTree(sub_left_preorder, sub_left_inorder);
+        
+    //     vector<int> sub_right_preorder(preorder.begin()+idx+1, preorder.end());
+    //     vector<int> sub_right_inorder(inorder.begin()+idx+1, inorder.end());
+    //     root->right = buildTree(sub_right_preorder, sub_right_inorder);
+    //     return root;
+    // }
+
+};
