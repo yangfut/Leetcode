b1324888c80570b1bcb56196a7fd320d3ed222a3
Daniel_Tseng <yangfut@uci.edu>
2024-04-06 00:32:33 +0800

    [208][C++] Update solution

--- c++/208_Implement-Trie-(Prefix-Tree).cpp
+++ c++/208_Implement-Trie-(Prefix-Tree).cpp
@@ -58,3 +58,65 @@ public:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
+
+
+class TrieNode{
+public:
+    TrieNode* children[26];
+    bool isWord;
+    TrieNode(){
+        for(int i=0;i<26;++i) children[i] = nullptr;
+        isWord=false;
+    }
+};
+class Trie {
+public:
+    
+    TrieNode* head;
+    Trie() {
+        head = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* curNode = head;
+
+        for(char c:word){
+            int idx=c-'a';
+            if(!curNode->children[idx]){
+                TrieNode* node = new TrieNode();
+                curNode->children[idx] = node;
+            }
+
+            curNode = curNode->children[idx];
+        }
+        curNode->isWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* curNode = head;
+        for(char c:word){
+            int idx=c-'a';
+            if(!curNode->children[idx]) return false;
+            curNode = curNode->children[idx];
+        }
+        return curNode->isWord;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* curNode = head;
+        for(char c:prefix){
+            int idx=c-'a';
+            if(!curNode->children[idx]) return false;
+            curNode = curNode->children[idx];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
