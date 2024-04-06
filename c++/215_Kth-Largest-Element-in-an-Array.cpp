5d7c0a60e6d4639c4708c6112a0210d32a1ebc01
Daniel_Tseng <yangfut@uci.edu>
2024-04-06 17:01:32 +0800

    [215][C++] Update Min Heap and QuickSelect solutions

--- c++/215_Kth-Largest-Element-in-an-Array.cpp
+++ c++/215_Kth-Largest-Element-in-an-Array.cpp
@@ -20,3 +20,35 @@ public:
         return ans-offset;
     }
 };
+
+class Solution {
+public:
+    // #1 Min Heap
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>, greater<int>> pq; //min heap
+        for(int ele:nums){
+            pq.push(ele);
+            if(pq.size()>k) pq.pop();
+        }
+
+        return pq.top();
+    }
+    // #2 Quick Select
+    // int quickSelect(vector<int>& nums, int l, int r, int k){
+    //     int ptr=l, pivot = nums[r];
+    //     for(int i=l;i<r;++i){
+    //         if(nums[i]<pivot){
+    //             swap(nums[i], nums[ptr]);
+    //             ++ptr;
+    //         }
+    //     }
+    //     swap(nums[ptr], nums[r]);
+    //     if(ptr>k) return quickSelect(nums, l, ptr-1, k);
+    //     else if(ptr<k) return quickSelect(nums, ptr+1, r, k);
+    //     return nums[ptr];
+    // }
+    // int findKthLargest(vector<int>& nums, int k) {
+    //     k = nums.size()-k;
+    //     return quickSelect(nums, 0, nums.size()-1, k);
+    // }
+};
