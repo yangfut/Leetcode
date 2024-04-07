5b6ac8c4e18eb4753b5993b29388d0ee148fefee
Daniel_Tseng <yangfut@uci.edu>
2024-04-07 20:09:21 +0800

    [78][C++] Update solution

--- c++/78_Subsets.cpp
+++ c++/78_Subsets.cpp
@@ -26,3 +26,29 @@ public:
             return ans;
         }
 };
+
+class Solution {
+public:
+    void backtracking(vector<int>& nums, int idx, vector<int>& buffer, vector<vector<int>>& result){
+        if(idx == nums.size()){
+            result.push_back(buffer);
+            return;
+        }
+
+        // Pick
+        buffer.push_back(nums[idx]);
+        backtracking(nums, idx+1, buffer, result);
+
+        // Not Pick
+        buffer.pop_back();
+        backtracking(nums, idx+1, buffer, result);
+
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> buffer;
+        backtracking(nums, 0, buffer, result);
+        return result;
+    }
+};
