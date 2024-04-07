3df085a8802fe245ad621807ffc873ba356c22cf
Daniel_Tseng <yangfut@uci.edu>
2024-04-07 15:39:54 +0800

    [55][C++] Update the optimized solution and the naive one

--- c++/55_Jump-Game.cpp
+++ c++/55_Jump-Game.cpp
@@ -14,3 +14,41 @@ public:
         return dp[nums.size()-1]>=nums.size()-1;
     }
 };
+
+class Solution {
+public:
+    // #1 DFS + Memoization
+    // bool dfs(vector<int>& nums, int start, vector<int>& memo){
+        
+    //     // Base case
+    //     if(start>=nums.size()-1) return true;
+    //     if(memo[start]!=-1) return memo[start];
+
+    //     int jump = nums[start];
+    //     if(jump==0) return false;
+
+    //     for(int next=start+1; next<=start+jump; ++next){
+    //         if(dfs(nums, next, memo)){
+    //             memo[start]=1;
+    //             return true;
+    //         }
+    //     }
+    //     memo[start]=0;
+    //     return false;
+    // }
+    
+    // bool canJump(vector<int>& nums) {
+    //     vector<int>memo(nums.size(), -1);
+    //     return dfs(nums, 0, memo);
+    // }
+    
+    // #2 Dynamic Programming to find the maximum reach point
+    bool canJump(vector<int>& nums) {
+        int i=0;
+        for(int reach=0; i<nums.size()&&i<=reach;++i){
+            reach = max(reach, i+nums[i]);
+        }
+
+        return i==nums.size();
+    }
+};
