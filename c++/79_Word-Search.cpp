9c1bcab05ba22078d2bb0ce16be344d1834bde30
Daniel_Tseng <yangfut@uci.edu>
2024-04-09 23:48:09 +0800

    [79][C++] Update solution

--- c++/79_Word-Search.cpp
+++ c++/79_Word-Search.cpp
@@ -32,3 +32,34 @@ public:
         return dfs_sol(board, word);
     }
 };
+
+class Solution {
+public:
+    bool dfs(vector<vector<char>>& board, int row, int col, string word, int idx){
+        if(row<0||col<0||row==board.size()||col==board[0].size()||board[row][col]!=word[idx]) return false;
+        if(idx==word.size()-1) return true;
+
+        char temp = board[row][col];
+        board[row][col] = '@'; // visisted
+
+        bool right = dfs(board, row+1, col, word, idx+1);
+        bool left  = dfs(board, row-1, col, word, idx+1);
+        bool up = dfs(board, row, col+1, word, idx+1);
+        bool down = dfs(board, row, col-1, word, idx+1);
+
+        board[row][col] = temp;
+        return right || left || up || down;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        for(int row=0;row<m;++row){
+            for(int col=0;col<n;++col){
+                if(board[row][col]!=word[0]) continue;
+                if(dfs(board, row, col, word, 0)) return true;
+            }
+        }
+        return false;
+    }
+};
