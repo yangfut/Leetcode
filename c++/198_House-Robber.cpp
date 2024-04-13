ee18ea3650f2f49241bfcbcb316b9a116c9e3fe6
Daniel_Tseng <yangfut@uci.edu>
2024-04-13 16:59:45 +0800

    [198][C++] Update optimized solution

--- c++/198_House-Robber.cpp
+++ c++/198_House-Robber.cpp
@@ -1,14 +1,34 @@
 class Solution {
+private:
+    int tabulation(vector<int>& nums){
+        int prevMax=0, n=nums.size();
+        vector<int>tab(n, 0);
+        for(int houseIdx=0; houseIdx<n; ++houseIdx){
+            if(houseIdx>=2) prevMax=max(prevMax, tab[houseIdx-2]);
+            tab[houseIdx] = nums[houseIdx] + prevMax;
+        }
+        return (n>1)?max(tab.back(), tab[n-2]):tab.back();
+    }
+
+    int _recursion(vector<int>& nums, vector<int>& memo, int idx){
+        if(idx<0) return 0;
+        if(memo[idx]==-1){
+            memo[idx] = max(_recursion(nums, memo, idx-1), _recursion(nums, memo, idx-2)+nums[idx]);
+        }
+        return memo[idx];
+    }
+    int memoization(vector<int>& nums){
+        int n=nums.size();
+        vector<int>memo(n, -1);
+
+        return _recursion(nums, memo, n-1);
+    }
 public:
     int rob(vector<int>& nums) {
-        int cur=nums[0], prev=0;
+        // #1 Tabulation
+        // return tabulation(nums);
 
-        for(int i=1;i<nums.size();i++){
-            int new_max=max(prev+nums[i], cur);
-            prev=cur;
-            cur=new_max;
-        }
-
-        return cur>prev?cur:prev;
+        // #2 Memoization
+        return memoization(nums);
     }
 };
