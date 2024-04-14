68f46e6ae7f7d86ece2892129c46b020de11cb1c
Daniel_Tseng <yangfut@uci.edu>
2024-04-14 22:40:12 +0800

    [152][C++] Update another solution

--- c++/152_Maximum-Product-Subarray.cpp
+++ c++/152_Maximum-Product-Subarray.cpp
@@ -24,3 +24,42 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        vector<int>leftProduct(n);
+        vector<int>rightProduct(n);
+
+        leftProduct[0]=nums[0];
+        for(int i=1; i<n; ++i){
+            if(leftProduct[i-1]==0){
+                // store the result excluding ZERO in the previous index
+                leftProduct[i]=nums[i];
+
+            }else{
+                leftProduct[i]=leftProduct[i-1]*nums[i];
+            }
+            
+        }
+
+        rightProduct[n-1]=nums[n-1];
+        for(int i=n-2; i>=0; --i){
+            if(rightProduct[i+1]==0){
+                // store the result exlucding ZERO in the next index
+                rightProduct[i]=nums[i];
+            }else{
+                rightProduct[i]=rightProduct[i+1]*nums[i];
+            }
+        }
+
+        int maxp;
+        for(int i=0; i<n; ++i){
+            int maxp_temp = max(leftProduct[i], rightProduct[i]);
+            maxp = max(maxp, maxp_temp);
+        }
+        return maxp;
+    }
+};
