e043794161f0ff68a1395fdf25db6086cdecee3a
Daniel_Tseng <yangfut@uci.edu>
2024-04-17 00:21:04 +0800

    [300][C++] Refresh this question

--- c++/300_Longest-Increasing-Subsequence.cpp
+++ c++/300_Longest-Increasing-Subsequence.cpp
@@ -76,3 +76,88 @@ public:
 
     }
 };
+
+class Solution {
+public:
+    int dfsHelper(const vector<int>& nums, vector<int>& cache, int currIdx, int nextIdx){
+        if(nextIdx == nums.size()) return 1;
+
+        int seqLen = 0;
+        for(int idx = nextIdx; idx < nums.size(); ++idx){
+            // monotonic increasing
+            if(nums[currIdx] >= nums[idx]) continue;
+            if(cache[idx] == -1) cache[idx] = dfsHelper(nums, cache, idx, idx+1);
+            seqLen = max(seqLen, cache[idx]);
+        }
+
+        cache[currIdx] = seqLen + 1;
+        return cache[currIdx];
+    }
+    // Create decision tree for every iteration
+    // Use cache to store the optimal solution(LIS)
+    // To get optimal solution, must calculate all the sub sequences 
+    int dfs_cache_sol(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return 1;
+
+        vector<int>cache(n, -1);
+        int maxv = 0;
+
+        for(int currIdx = 0; currIdx < n; ++currIdx){
+            if(cache[currIdx] == -1) cache[currIdx] = dfsHelper(nums, cache, currIdx, currIdx+1);
+            maxv = max(maxv, cache[currIdx]);
+        }
+        return maxv;
+    }
+
+    // Use tabulation method to count LIS from last element, which must be 1
+    // Each current idx must look forward to find the max value in its subseq
+    int tabulation_solution(vector<int>& nums){
+        int n = nums.size();
+        if(n == 1) return 1;
+
+        vector<int> dp(n, 1);
+        int LIS = 0;
+        for(int l = n-2; l >=0; --l){
+            for(int r = l+1; r < n; ++r){
+                if(nums[l] >= nums[r]) continue;
+                dp[l] = max(dp[l], dp[r]+1);
+            }
+        }
+
+        for(int& length : dp) LIS = max(LIS, length);
+        return LIS;
+    }
+
+    int binarySearch_solution(vector<int>& nums) {
+        // Edge case
+        if(nums.size()==1) return 1;
+        int n = nums.size();
+        vector<int> subSeq;
+        subSeq.push_back(nums[0]);
+
+        for(int i=1; i<n; ++i){
+            if(subSeq.back() < nums[i]) subSeq.push_back(nums[i]);
+            else{
+                int idx = upper_bound(subSeq.begin(), subSeq.end(), nums[i]) - subSeq.begin();
+                if(idx == 0 || subSeq[idx-1] != nums[i]){
+                    subSeq[idx] = nums[i];
+                }
+            }
+        }
+        return subSeq.size();
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        // 1. DFS with caching
+        // TC: O(N^2), SC:O(N)
+        // return dfs_cache_sol(nums);
+
+        // 2. Dynamic Programming with Tabulation
+        // TC: O(N^2), SC:O(N)
+        // return tabulation_solution(nums);
+
+        // 3. Binary Search
+        // TC:O(NlogN), SC:O(N)
+        return binarySearch_solution(nums);
+    }
+};
