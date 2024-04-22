99ded26d3a63da7e07d4db5b58933bc80882212d
Daniel_Tseng <yangfut@uci.edu>
2024-04-23 01:28:21 +0800

    [62][C++] Update another solution

--- c++/62_Unique-Path.cpp
+++ c++/62_Unique-Path.cpp
@@ -12,3 +12,21 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        // Edge cases:
+        if(m == 1 || n == 1) return 1;
+
+        vector<vector<int>> memo(m, vector<int>(n, 1));
+
+        // memo[m][n] = memo[m-1][n] + memo[m][n-1];
+        for(int row = 1; row < m; ++row){
+            for(int col = 1; col < n; ++col){
+                memo[row][col] = memo[row-1][col] + memo[row][col-1];
+            }
+        }
+        return memo.back().back();
+    }
+};
