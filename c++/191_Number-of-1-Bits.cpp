478d2304fb954bcf2faa79780463a53e77e9a3ec
Daniel_Tseng <yangfut@uci.edu>
2024-04-30 02:44:25 +0800

    [191][C++] Update bitset and optimal solution

--- c++/191_Number-of-1-Bits.cpp
+++ c++/191_Number-of-1-Bits.cpp
@@ -9,3 +9,20 @@ public:
         return weight;
     }
 };
+
+class Solution {
+public:
+    int hammingWeight(int n) {
+        // #1 bitset
+        bitset<32> bits(n);
+        return bits.count();
+
+        // #2 Division
+        // int hamming = 0;
+        // while(n > 0){
+        //     if(n % 2) ++hamming;
+        //     n /= 2;
+        // }
+        // return hamming;
+    }
+};
