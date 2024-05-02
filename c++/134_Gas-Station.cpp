3e57a3fa8c09e39da9af6baaf675b8af1f06e9d8
Daniel_Tseng <yangfut@uci.edu>
2024-05-02 23:17:25 +0800

    [134][C++] Update other solution

--- c++/134_Gas-Station.cpp
+++ c++/134_Gas-Station.cpp
@@ -15,3 +15,33 @@ public:
         return (sum>=0)?st:-1;
     }
 };
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int n = gas.size(), sum = 0;
+        vector<int> remain(n);
+        for(int idx = 0; idx < n; ++idx) {
+            remain[idx] = gas[idx] - cost[idx];
+            sum += remain[idx];
+        }
+
+        if(sum < 0) return -1;
+
+        // if the current index is the proper starting index, then all the traversal must be greater than zero
+        // otherwise, pick the index next to the negative sum
+
+        int startIdx = 0; 
+        sum = 0;
+        for(int idx = 0; idx < n; ++idx){
+            sum += remain[idx];
+            if(sum < 0){
+                startIdx = idx + 1;
+                sum = 0;
+            }
+        }
+
+        return startIdx;
+
+    }
+};
