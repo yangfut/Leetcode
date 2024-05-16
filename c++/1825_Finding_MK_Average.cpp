66852618af0748a01224624c3cc47d3548ae400d
Daniel_Tseng <yangfut@uci.edu>
2024-05-17 01:49:24 +0800

    [1825][C++] Update another solution

--- c++/1825_Finding_MK_Average.cpp
+++ c++/1825_Finding_MK_Average.cpp
@@ -76,3 +76,78 @@ public:
  * obj->addElement(num);
  * int param_2 = obj->calculateMKAverage();
  */
+
+class MKAverage {
+private:
+    int m, k;
+    multiset<int> topKSmall, topKBig, middle;
+    deque<int> deq;
+    long sum;
+public:
+    MKAverage(int _m, int _k) {
+        m = _m;
+        k = _k;
+        sum = 0;
+    }
+
+    ~MKAverage(){
+
+    }
+
+    void addElement(int num) {
+        
+        if(deq.size() == m){
+            int numToRemove = deq.front();
+            deq.pop_front();
+
+            if(numToRemove <= *prev(topKSmall.end())){
+                auto it = topKSmall.find(numToRemove);
+                topKSmall.erase(it);
+                it = middle.begin();
+                topKSmall.insert(*it);
+                sum -= *it;
+                middle.erase(it);
+            }else if(numToRemove >= *topKBig.begin()){
+                auto it = topKBig.find(numToRemove);
+                topKBig.erase(it);
+                it = prev(middle.end());
+                topKBig.insert(*it);
+                sum -= *it;
+                middle.erase(it);
+            }else{
+                sum -= numToRemove;
+                middle.erase(numToRemove);
+            }
+        }
+
+        deq.push_back(num);
+
+        // Adjust multiset structures to accommodate the new element
+        topKSmall.insert(num);
+        if(topKSmall.size() <= k) return;
+
+        // take the last element to topKBig
+        auto it = prev(topKSmall.end());
+        topKBig.insert(*it);
+        topKSmall.erase(it);
+        if(topKBig.size() <= k) return;
+        
+        // take the first element to middle
+        it = topKBig.begin();
+        middle.insert(*it);
+        sum += *it;
+        topKBig.erase(it);
+    }
+    
+    int calculateMKAverage() {
+        if(deq.size() < m) return -1;
+        return sum / static_cast<long>(m-2*k);
+    }
+};
+
+/**
+ * Your MKAverage object will be instantiated and called as such:
+ * MKAverage* obj = new MKAverage(m, k);
+ * obj->addElement(num);
+ * int param_2 = obj->calculateMKAverage();
+ */
