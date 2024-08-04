25deb30d93abddf72c275f8d66f8e3771b92ff62
Daniel_Tseng <yangfut@uci.edu>
2024-08-04 14:17:16 +0800

    [746][C++] Update solution with memoization

--- c++/746_Min-Cost-Climbing-Stairs.cpp
+++ c++/746_Min-Cost-Climbing-Stairs.cpp
@@ -12,3 +12,17 @@ public:
         return dp[n];
     }
 };
+
+// Tabulation
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> memo = vector<int>(cost.size()+1, -1);
+        memo[0] = 0;
+        memo[1] = 0;
+        for(int i = 2; i <= cost.size(); ++i){
+            memo[i] = min(memo[i-1]+cost[i-1], memo[i-2]+cost[i-2]);
+        }
+        return memo.back();
+    }
+};
