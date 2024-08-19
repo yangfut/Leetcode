547a4b4f2de12c06608fb44a372efdddbe03c881
Daniel_Tseng <yangfut@uci.edu>
2024-08-19 22:49:20 +0800

    [118][C++] Pascal's Triangle

--- c++/118_Pascal's-Triangle.cpp
+++ c++/118_Pascal's-Triangle.cpp
@@ -17,3 +17,21 @@ public:
         return tri;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> output;
+
+        for(int i = 0; i < numRows; ++i){
+            vector<int> one_line(i+1, 1);
+            if(i>=2){
+                for(int j = 1; j < i; ++j){
+                    one_line[j] = output[i-1][j-1]+ output[i-1][j];
+                }
+            }
+            output.push_back(one_line);
+        }
+        return output;
+    }
+};
