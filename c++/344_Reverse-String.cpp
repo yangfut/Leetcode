9bd11b1b4a0330cbca5d1290528b2b7ce341cb1f
Daniel_Tseng <yangfut@uci.edu>
2025-01-06 21:42:59 +0800

    [344][C++] Update another solution

--- c++/344_Reverse-String.cpp
+++ c++/344_Reverse-String.cpp
@@ -4,3 +4,13 @@ public:
         reverse(s.begin(), s.end());
     }
 };
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        for(int i = 0; i <= (n-1-i); ++i){
+            swap(s[i], s[n-1-i]);
+        }
+    }
+};
