ff65b3a69c067deffc3dddb351e5d214fbe769ba
Daniel_Tseng <yangfut@uci.edu>
2026-02-08 23:39:08 +0800

    [518][C++] Coin Change II

--- c++/518_Coin-Change-II.cpp
+++ c++/518_Coin-Change-II.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        vector<uint32_t> dp(amount+1,0);
+        dp[0] = 1;
+        for(int coin : coins){
+            for(int i = 1; i <= amount; ++i){
+                if(i - coin < 0) continue;
+                dp[i] += dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
+};
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
         if(amount == 0) return 1;
         vector<int>dp(amount+1, 0);
         dp[0] = 1;
