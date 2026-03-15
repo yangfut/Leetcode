5550a2c7534185a5dd1304d9521f799806d795ae
Daniel_Tseng <yangfut@uci.edu>
2026-03-15 22:51:45 +0800

    [146][C++] Update solution with std::list

--- c++/146_LRU-Cache.cpp
+++ c++/146_LRU-Cache.cpp
@@ -176,3 +176,45 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
+ class LRUCache {
+public:
+    // first: key, second: value
+    list<pair<int,int>> cache;
+    unordered_map<int,list<pair<int,int>>::iterator> map;
+    int cap;
+    LRUCache(int capacity) : cap(capacity) {}
+    
+    int get(int key) {
+        auto it = map.find(key);
+        if(it == map.end()) return -1;
+        cache.splice(cache.begin(), cache, it->second);
+        it->second = cache.begin();
+        return it->second->second;
+    }
+    
+    void put(int key, int value) {
+        auto it = map.find(key);
+        if(it != map.end()){
+            it->second->second = value;
+            cache.splice(cache.begin(), cache, it->second);
+            return;
+        }
+
+        if(map.size() == cap){
+            auto [lastKey, lastVal] = cache.back();
+            map.erase(lastKey);
+            cache.pop_back();
+        }
+
+        cache.emplace_front(key, value);
+        map[key] = cache.begin();
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
