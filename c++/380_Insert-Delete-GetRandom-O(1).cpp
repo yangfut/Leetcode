5b8a26b8b2d6abfcf19690d48399ea4822fe24e5
Daniel_Tseng <yangfut@uci.edu>
2026-03-15 21:46:04 +0800

    [380][C++] Update solution

--- c++/380_Insert-Delete-GetRandom-O(1).cpp
+++ c++/380_Insert-Delete-GetRandom-O(1).cpp
@@ -113,3 +113,41 @@ public:
  * bool param_2 = obj->remove(val);
  * int param_3 = obj->getRandom();
  */
+
+ class RandomizedSet {
+public:
+    unordered_map<int,int> map;
+    vector<int> vec;
+    RandomizedSet() {
+
+    }
+    
+    bool insert(int val) {
+        if(map.count(val)) return false;
+        map[val] = vec.size();
+        vec.emplace_back(val);
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(!map.count(val)) return false;
+        int idx = map[val];
+        map[vec.back()] = idx;
+        swap(vec[idx], vec.back());
+        vec.pop_back();
+        map.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return vec[rand() % vec.size()];
+    }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
