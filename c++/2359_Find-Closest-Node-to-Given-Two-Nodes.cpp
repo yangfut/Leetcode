93f3e72da5a366d7a12c7e660a7033164302af13
Daniel_Tseng <yangfut@uci.edu>
2026-07-04 17:33:13 +0800

    [2359][C++] Find Closest Node to Given Two Nodes

---
+++ c++/2359_Find-Closest-Node-to-Given-Two-Nodes.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        // minimum of max(dist1[x], dist2[x])
+        int n = edges.size();
+        vector<vector<int>> dist(n, vector<int>(2,-1));
+        vector<int> nodes = {node1, node2};
+        int distance;
+        for(int i = 0; i < 2; ++i){
+            distance = -1;
+            queue<int> q;
+            q.push(nodes[i]);
+            while(!q.empty() && q.front() != -1){
+                ++distance;
+                int node = q.front(); q.pop();
+                if(dist[node][i] == -1){
+                    dist[node][i] = distance;
+                    q.push(edges[node]);
+                }
+            }
+        }
+
+        int minDist = INT_MAX, index = -1;
+        for(int i = 0; i < n; ++i){
+            if(dist[i][0] == -1 || dist[i][1] == -1) continue;
+            int currDist = max(dist[i][0],dist[i][1]);
+            if(currDist < minDist){
+                index = i;
+                minDist = currDist;
+            }
+        }
+        return index;
+    }
+};
