60d654dba8741d8dd0eec24444bb487e2d2fe8b6
Daniel_Tseng <yangfut@uci.edu>
2026-07-12 17:09:29 +0800

    [72][C++] mock: better solution

--- c++/72_Edit-Distance.cpp
+++ c++/72_Edit-Distance.cpp
@@ -64,3 +64,22 @@ public:
         return tab_solution(word1, word2);
     }
 };
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.length(), m = word2.length();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        // padding: add numbers linearly
+        for(int i = 0; i < n; ++i) dp[i+1][0] = i+1;
+        for(int j = 0; j < m; ++j) dp[0][j+1] = j+1;
+
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < m; ++j){
+                if(word1[i] == word2[j]) dp[i+1][j+1] = dp[i][j];
+                else dp[i+1][j+1] = 1 + min(dp[i][j+1], min(dp[i+1][j], dp[i][j]));
+            }
+        }
+        return dp[n][m];
+    }
+};
