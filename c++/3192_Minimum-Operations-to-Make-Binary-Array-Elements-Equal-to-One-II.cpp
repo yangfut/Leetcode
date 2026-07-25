334e06863eb11219a9e3388f44be43d5ad30328e
Daniel_Tseng <yangfut@uci.edu>
2026-07-25 22:52:54 +0800

    [3192][C++] Minimum Operations to Make Binary Array Elements Equal to One II

---
+++ c++/3192_Minimum-Operations-to-Make-Binary-Array-Elements-Equal-to-One-II.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int flips = 0, n = nums.size();
+        for(int i = 0; i < n; ++i){
+            if((nums[i] + flips) % 2 == 1){
+                nums[i] = 0;
+            }else{
+                nums[i] = 1;
+            }
+            flips += nums[i];
+        }
+        return flips;
+    }
+};
+
+// Improve division by using XOR operation to find odd/even number
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int flips = 0, n = nums.size();
+        for(int i = 0; i < n; ++i){
+            // find odd: (nums[i] + flips) % 2 == 1
+            // find odd: (nums[i] ^ flips) & 1 == 1
+
+            // find even: (nums[i] + flips) % 2 == 0
+            // find even: (nums[i] ^ flips) & 1 == 0
+            if((nums[i] ^ flips) & 1){
+                nums[i] = 0;
+            }else{
+                nums[i] = 1;
+            }
+            flips += nums[i];
+        }
+        return flips;
+    }
+};
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int flips = 0, n = nums.size();
+        for(int i = 0; i < n; ++i){
+            if((nums[i] ^ flips) & 1) continue;
+            ++flips;
+        }
+        return flips;
+    }
+};
