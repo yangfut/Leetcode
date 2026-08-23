0bf907f6df3f8adcba22de4e6421c96320a29fd2
Daniel_Tseng <yangfut@uci.edu>
2026-08-24 00:16:28 +0800

    [918][C++] Update sliding window and prefix solutions

--- c++/918_Maximum-Sum-Circular-Subarray.cpp
+++ c++/918_Maximum-Sum-Circular-Subarray.cpp
@@ -27,3 +27,73 @@ public:
         return max(globalMax, totalSum - globalMin);
     }
 };
+
+// Greedy (Kadane's algorithm)
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        // Consider wrap and non-wrap case
+        int loMax = 0, glMax = INT_MIN, loMin = 0, glMin = INT_MAX;
+        int sum = 0;
+        for(int ele : nums){
+            sum += ele;
+
+            loMax = max(loMax, 0) + ele;
+            loMin = min(loMin, 0) + ele;
+
+            glMax = max(loMax, glMax);
+            glMin = min(loMin, glMin);
+        }
+
+        if(sum - glMin == 0) return glMax;
+        return max(glMax, sum - glMin);
+    }
+};
+
+// Sliding Window in size of n
+// TC: O(N), SC:O(N)
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix (2*n+1, 0);
+        for(int i = 0; i < 2*n; ++i){
+            prefix[i+1] = prefix[i] + nums[i % n];
+        }
+
+        deque<int> deq;
+        deq.push_back(0);
+        int res = INT_MIN;
+        
+        for(int i = 1; i <= 2*n; ++i){
+            // expired starting point
+            while(!deq.empty() && i - deq.front() > n) deq.pop_front();
+            res = max(res, prefix[i] - prefix[deq.front()]);
+            // Find minimum sum
+            while(!deq.empty() && prefix[i] <= prefix[deq.back()]) deq.pop_back();
+            deq.push_back(i);
+        }
+        return res;
+    }
+};
+
+// Brute force with prefix sums
+// TC: O(N^2), SC: O(M)
+class Solution {
+public:
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(2*n+1, 0);
+        for(int i = 0; i < 2*n; ++i){
+            prefix[i+1] = prefix[i] + nums[i%n];
+        }
+        
+        int res = INT_MIN;
+        for(int i = 0; i <= 2*n; ++i){
+            for(int j = i+1; j <= 2*n && j-i <= n; ++j){
+                res = max(res, prefix[j] - prefix[i]);
+            }
+        }
+        return res;
+    }
+};
