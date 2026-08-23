8ac280fd906e1749e89fa07263966a09a1792d30
Daniel_Tseng <yangfut@uci.edu>
2026-08-24 00:57:25 +0800

    [53][C++] Update sliding window solution

--- c++/53_Maximum-Subarray.cpp
+++ c++/53_Maximum-Subarray.cpp
@@ -1,3 +1,5 @@
+// DP
+// TC:O(N), SC:O(1)
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -9,5 +11,28 @@ public:
             maxv = max(maxt, maxv);
         }
         return maxv;
+    }
+};
+
+// Sliding window
+// TC: O(N), SC: O(N)
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefix(n+1, 0);
+        for(int i = 0; i < n; ++i) prefix[i+1] = prefix[i] + nums[i];
+
+        deque<int> deq;
+        deq.push_back(0);
+        int res = INT_MIN;
+
+        // Maximum subarray[i:j] = maximum of prefix[j] - prefix[i]
+        for(int i = 1; i <= n; ++i){
+            res = max(res, prefix[i] - prefix[deq.front()]);
+            while(!deq.empty() && prefix[i] < prefix[deq.back()]) deq.pop_back();
+            deq.push_back(i);
+        }
+        return res;
     }
 };
