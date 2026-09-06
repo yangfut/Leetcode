6dc597f5359f53c4b3e21123ec89b8f2a002820a
Daniel_Tseng <yangfut@uci.edu>
2026-09-06 13:41:08 +0800

    [221][C++] Update monotonic stack and 2D-DP solution

--- c++/221_Maximal-Sequare.cpp
+++ c++/221_Maximal-Sequare.cpp
@@ -21,3 +21,64 @@ public:
     return maxSqr * maxSqr;
     }
 };
+
+// Monotonic stack
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        int maxSeq = 0;
+        vector<int> acc(m,0);
+        for(int r = 0; r < n; ++r){
+            for(int c = 0; c < m; ++c){
+                if(matrix[r][c] == '1') acc[c] += 1;
+                else acc[c] = 0;
+            }
+
+            stack<int>st;
+            vector<int>nls(m,m);
+            vector<int>pls(m,-1);
+            // nls
+            for(int c = m-1; c >= 0; --c){
+                while(!st.empty() && acc[st.top()] >= acc[c]) st.pop();
+                if(!st.empty()) nls[c] = st.top();
+                st.push(c);
+            }
+
+            while(!st.empty()) st.pop();
+            // pls
+            for(int c = 0; c < m; ++c){
+                while(!st.empty() && acc[st.top()] >= acc[c]) st.pop();
+                if(!st.empty()) pls[c] = st.top();
+                st.push(c);
+            }
+
+            for(int c = 0; c < m; ++c){
+                maxSeq = max(maxSeq, min(nls[c] - pls[c] - 1, acc[c]));
+            }
+        }
+        return maxSeq * maxSeq;
+    }
+};
+
+// 2D-DP
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        int maxSide = 0;
+        // dp: store the max side of square
+        // To expand the square by one, need to determine the square from left, top, diagonal
+        for(int r = 1; r <= n; ++r){
+            for(int c = 1; c <= m; ++c){
+                if(matrix[r-1][c-1] == '1'){
+                    int tempMin = min(min(dp[r-1][c], dp[r][c-1]), dp[r-1][c-1]);
+                    dp[r][c] = tempMin + 1;
+                }
+                maxSide = max(dp[r][c], maxSide);   
+            }
+        }
+        return maxSide * maxSide;
+    }
+};
