258f0276c74f86f90f2279f1b81b02be5a9b485b
Daniel_Tseng <yangfut@uci.edu>
2026-09-14 00:42:22 +0800

    [907][C++] Sum of Subarray Minimums

--- c++/907_Sum-of-Subarray-Minimums.cpp
+++ c++/907_Sum-of-Subarray-Minimums.cpp
@@ -23,3 +23,64 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        int mod = 1000000007;
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        for(int i = 0; i < n; ++i){
+            int lmin = arr[i];
+            for(int j = i; j < n; ++j){
+                lmin = min(lmin, arr[j]);
+                dp[i][j] = lmin;
+            }
+        }
+
+        int sum = 0;
+        for(int i = 0; i < n; ++i){
+            for(int j = i; j < n; ++j){
+                sum += dp[i][j];
+                sum %= mod;
+            }
+        }
+        return sum;
+    }
+};
+
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        int mod = 1e9+7;
+        long long sum = 0;
+        vector<int> left(n,-1), right(n,n);
+        stack<int> st;
+        // next less element
+        for(int i = n-1; i >= 0; --i){
+            while(!st.empty() && arr[i] < arr[st.top()]) st.pop();
+            if(!st.empty()){
+                right[i] = st.top();
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+        // previous less element
+        for(int i = 0; i < n; ++i){
+            while(!st.empty() && arr[i] <= arr[st.top()]) st.pop();
+            if(!st.empty()){
+                left[i] = st.top();
+            }
+            st.push(i);
+        }
+
+        for(int i = 0; i < n; ++i){
+            int ldist = i - left[i];
+            int rdist = right[i] - i;
+            sum = (sum + ((long long)arr[i] * ldist * rdist)) % mod;
+        }
+        return sum;
+    }
+};
